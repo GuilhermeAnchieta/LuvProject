8be55198168ef5ef8ab57770a9e69673cb9ced9a
GuilhermeAnchieta <guilherme.anchietass@gmail.com>
2025-02-01 20:28:51 -0300

    initialize Android and iOS project structure with Capacitor configuration and essential files

---
+++ android/app/src/main/java/com/LoveProject/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.LoveProject.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.LoveProject.app',
+  appName: 'LoveProject',
+  webDir: 'dist/projeto-maria/browser'
+};
+
+export default config;
