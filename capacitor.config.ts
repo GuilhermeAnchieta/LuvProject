7e40f8d26d7861a13f96c847ffd9122e48b0ee34
GuilhermeAnchieta <guilherme.anchietass@gmail.com>
2025-02-13 00:35:39 -0300

    feat: update app branding and icons, migrate to WebP format

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.LoveProject.app',
-  appName: 'LoveProject',
+  appName: 'loveMaria',
   webDir: 'dist/projeto-maria/browser'
 };
 
